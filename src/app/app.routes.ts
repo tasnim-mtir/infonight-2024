3bbab53bf31733ee38c5260af935888a52101c6c
tesnim-mtir <tasnim.mtir@esprim.tn>
2024-12-06 00:45:55 +0100

    !

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { ResultComponent } from './result/result.component';
 import { MainQuizComponent } from './main-quiz/main-quiz.component';
+import { FlashcardsComponent } from './flashcards/flashcards.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'quiz', component: MainQuizComponent },
+    { path: 'flashcard', component: FlashcardsComponent },
+
+
 
 
 
---
+++ src/app/flashcards/flashcards.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-flashcards',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './flashcards.component.html',
+  styleUrl: './flashcards.component.scss'
+})
+export class FlashcardsComponent {
+
+}
